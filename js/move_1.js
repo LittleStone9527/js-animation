f2ea4406ae9b40da9abb22e4df250560b2194b16
LittleStone9527 <littlestone9527@126.com>
2015-06-03 16:06:52 +0800

    js animation beta_4
    if (!document.getElementById("pic")) return false;
    if (!document.getElementById("link_list")) return false;
    
    傻逼啊

--- js/move_1.js
+++ js/move_1.js
@@ -51,19 +51,45 @@ function moveMsg(elemId,finalX,finalY,interval){
 	elem.movement = setTimeout(repeat,interval);
 	//movement = setTimeout("moveMsg()",10);
 }
+// 在后面插入元素
+function insertAfter(newEle,targetEle){
+	var parentEle = targetEle.parentNode;
 
-function posMsg(){
-	if (!document.getElementById("pic")) return false;
-	var oImg = document.getElementById("pic");
-	oImg.style.position = "absolute";
-	oImg.style.left = "0px";
-	oImg.style.top = "0px";
+	if (parentEle.lastChild == targetEle) {
+		parentEle.appendChild(newEle);
+	}
+	else{
+		parentEle.insertBefore(newEle,targetEle.nextSibling);
+	}
+}
 
-	var alinks = document.getElementsByTagName("a");
-	// console.log(alinks);
+
+function prepareSlideshow(){
+	//确保浏览器理解DOM方法
+	if (!document.getElementsByTagName) return false;
+	if (!document.getElementById) return false;
+	//确保元素存在
+	if(!document.getElementById("linklist")) return false;
+
+	//创建div
+	var slideshow = document.createElement("div");
+	slideshow.setAttribute("id","slideshow");
+
+	//创建img
+	var pic = document.createElement("img");
+	pic.setAttribute("id","pic");
+	pic.setAttribute("src","image/showPic.jpg");
+	pic.setAttribute("alt","show pic");
+
+	//将img插入div
+	slideshow.appendChild(pic);
+
+	var list = document.getElementById("linklist");
+	insertAfter(slideshow,list);
+
+	var alinks = list.getElementsByTagName("a");
 	alinks[0].onmouseover = function(){
 		moveMsg("pic",0,0,10);
-		// console.log("ok");
 	}
 	alinks[1].onmouseover = function(){
 		moveMsg("pic",-100,0,10);
@@ -74,24 +100,8 @@ function posMsg(){
 	alinks[3].onmouseover = function(){
 		moveMsg("pic",-300,0,10);
 	}
-
 }
-
-function addonloadEvent(func){
-	var oldonload = window.onload;
-	if(typeof window.onload != 'function'){
-		window.onload = func;
-	}
-	else{
-		window.onload = function(){
-			oldonload();
-			func();
-		}
-	}
-}
-
-addonloadEvent(posMsg);
-// addonloadEvent(moveMsg);
+addonloadEvent(prepareSlideshow);
 
 
 
